58ea337fb25c5ed95652aa4e2a9a06d5f5821997
djology <djology.w@gmail.com>
2021-11-26 09:51:34 +0800

    Initial commit

---
+++ src/main/java/djology/big/BigApplication.java
@@ -0,0 +1,19 @@
+package djology.big;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Desc:
+ * @Author: djology.w
+ * @Date: 2021/11/26 9:41
+ */
+
+@SpringBootApplication
+public class BigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigApplication.class, args);
+    }
+
+}
